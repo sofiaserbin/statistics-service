b55f2fcf54cc16e87886f128c995416fb8e818a6
Michael Koenig <koenigmichael2002@gmail.com>
2024-01-09 01:30:33 +0100

    Disconnect mqtt upon SIGINT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import MqttRequest from "mqtt-request"
 import { getMostUsedDentist, getNumberOfAppointmentsThisYear } from "./controllers/v1/user-statistics.js"
 import { numberOfSearches, numberOfTimeslotsAvailable } from "./controllers/v1/admin-statistics.js";
 
-const client = mqtt.connect(process.env.BROKER_URL)
+const client = mqtt.connect(process.env.BROKER_URL, { clean: true })
 
 MqttRequest.timeout = 5000;
 
@@ -20,4 +20,10 @@ mqttReq.response("$share/statistics-service/v1/statistics/timeslots-available/re
 
 client.on("connect", () => {
     console.log("scheduling-service connected to broker")
+});
+
+process.on('SIGINT', () => {
+    client.end(); // since we're using a clean session, this unsubscribes from all topics
+    console.log('Disconnected from MQTT broker');
+    process.exit();
 });
